7b8cf02be7c88594fa005c12a657031779b05a24
cllcnkrt <cllcnkrt@outlook.com>
2021-07-30 11:53:13 +0300

    navbar and button errors fixed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,30 +1,31 @@
-import "./Button.css";
-import React from "react";
+import React from 'react';
+import './Button.css';
 
-const STYLES = ["btn--primary", "btn--outline"];
+const STYLES = ['btn--primary', 'btn--outline'];
 
-const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
 
-const COLOR = ["primary", "blue", "red", "green"];
+const COLOR = ['primary', 'blue', 'red', 'green'];
 
 export const Button = ({
   children,
   type,
   onClick,
+  buttonStyle,
   buttonSize,
-  buttonSyle,
-  buttonColor,
+  buttonColor
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonSyle) ? buttonSyle : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
-  const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  const checkButtonColor = STYLES.includes(buttonColor) ? buttonColor : COLOR[0];
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
   return (
     <button
-      className={`btn ${checkButtonStyle}
-        ${checkButtonSize} ${checkButtonColor}`}
+      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
       onClick={onClick}
       type={type}
     >
