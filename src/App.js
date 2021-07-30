ad7e614a850334d39810efe925d444f88ba9366f
cllcnkrt <cllcnkrt@outlook.com>
2021-07-30 14:36:30 +0300

    all pages done and project completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Footer from './components/pages/Footer/Footer';
 import Home from './components/pages/HomePage/Home';
- function App() {
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer/Footer';
+
+function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Switch>
-        <Route path='/' exact component={Home}/>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
       </Switch>
-      <Footer/>
+      <Footer />
     </Router>
-    
   );
 }
 
