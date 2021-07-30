29925b6dd1057f9e34416593411c4d745e318bc9
cllcnkrt <cllcnkrt@outlook.com>
2021-07-30 14:12:05 +0300

    data component

---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import {homeObjectOne} from './Data'
+
+function Home() {
+    return (
+        <>
+          <HeroSection {...homeObjectOne}/>  
+        </>
+    )
+}
+
+export default Home
